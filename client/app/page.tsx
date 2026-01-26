00d22179bd363ac16856007bddd59a443d2e8464
Deepakpmk007 <deepakpmk@hotmail.com>
2026-01-26 14:57:01 +0530

    add some change

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,7 +2,7 @@ import MainNav from "./components/layout/MainNav";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 h-screen bg-green-50">
+    <div className="flex flex-1 h-screen w-screen bg-green-50">
       <MainNav />
     </div>
   );
