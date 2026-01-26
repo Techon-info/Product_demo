b0f33fd74da3e736aa5e33c7d5e86ac4725cccc5
Deepakpmk007 <deepakpmk@hotmail.com>
2026-01-26 14:56:24 +0530

    add components nav bar

--- client/app/components/layout/MainNav.tsx
+++ client/app/components/layout/MainNav.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 export default function MainNav() {
   return (
     <nav className="h-16 flex flex-1 justify-between items-center p-5 bg-green-normal">
-      <h2 className="font-bold font-mono tracking-widest text-3xl text-white">
+      <h2 className="font-bold font-mono tracking-widest text-lg md:text-2xl lg:text-3xl text-white">
         <Link href={"/"}>Fresh Agri</Link>
       </h2>
-      <div className="flex lg:gap-10 text-white">
-        <ul className="flex gap-3 lg:text-xl">
+      <div className="xs:hidden sm:hidden md:flex lg:flex xl:flex 2xl:flex md:gap-5 lg:gap-10 text-white">
+        <ul className="flex gap-3 lg:text-lg xl:text-xl">
           <li>Featurs</li>
           <li>Products</li>
           <li>
@@ -16,7 +16,7 @@ export default function MainNav() {
           </li>
           <li>contact</li>
         </ul>
-        <div className="flex gap-5 lg:text-xl">
+        <div className="flex gap-5 md:gap-5 lg:text-xl">
           <Link href="/auth/login">Login</Link>
           <Link href="/auth/signup">Signup</Link>
         </div>
