1f474069ecc44cd92fde6f7e2c3d0df3780d09a9
Deepakpmk007 <deepakpmk@hotmail.com>
2026-01-26 14:56:50 +0530

    add some style

--- client/app/plans/page.tsx
+++ client/app/plans/page.tsx
@@ -5,10 +5,14 @@ export default function page() {
   return (
     <>
       <MainNav />
-      <div className="flex flex-col">
-        <h1>Flexible Plans for Every Farm Size</h1>
-        <h3>choose the perfert plan to grow your arricutural business</h3>
-        <div>
+      <div className="flex flex-col w-full lg:h-[93vh] bg-green-50 pt-20 items-center">
+        <h1 className="lg:text-6xl lg:font-bold">
+          Flexible Plans for Every Farm Size
+        </h1>
+        <h3 className="lg:text-3xl mt-5">
+          choose the perfert plan to grow your arricutural business
+        </h3>
+        <div className="flex gap-10 mt-20">
           <Card />
           <Card />
           <Card />
@@ -20,10 +24,10 @@ export default function page() {
 
 const Card = function () {
   return (
-    <div>
-      <h1>free</h1>
-      <h3>0/month</h3>
-      <ul>
+    <div className="flex lg:flex-col items-start gap-2 lg:w-72 bg-red-300 lg:p-10">
+      <h1 className="lg:text-2xl lg:font-bold">Free</h1>
+      <h3 className="lg:text-4xl tracking-wider">0/month</h3>
+      <ul className="lg:text-lg flex flex-col gap-3">
         <li>10 product</li>
         <li>Basic Vender Access</li>
         <li>Basic Vender</li>
