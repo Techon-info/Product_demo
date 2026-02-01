41c0e42a32ae536165d02271e3701fbf734a9d6f
Deepakpmk007 <deepakpmk@hotmail.com>
2026-02-01 13:03:22 +0530

    change some bg color

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "./components/layout/Hero";
 
 export default function HomePage() {
   return (
-    <main className="pt-16 bg-green-50 min-h-screen">
+    <main className="p-16 bg-gray-200 min-h-screen">
       <MainNav />
       <Hero />
     </main>
